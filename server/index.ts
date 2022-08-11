c8e20f726f9e9877f17dabfb9b26bd1fb199d05b
1emonislam <emonislamweb@gmail.com>
2022-08-11 11:25:20 +0600

    projects setups

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,4 @@
-import { createServer } from 'http'
-import { parse } from 'url'
+import express, { Request, Response } from 'express'
 import next from 'next'
 
 const port = parseInt(process.env.PORT || '3000', 10)
@@ -8,15 +7,14 @@ const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url!, true)
-    handle(req, res, parsedUrl)
-  }).listen(port)
+  const server = express()
 
-  // tslint:disable-next-line:no-console
-  console.log(
-    `> Server listening at http://localhost:${port} as ${
-      dev ? 'development' : process.env.NODE_ENV
-    }`
-  )
+  server.all('*', (req: Request, res: Response) => {
+    return handle(req, res)
+  })
+
+  server.listen(port, () => {
+    
+    console.log(`> Ready on http://localhost:${port}`)
+  })
 })
